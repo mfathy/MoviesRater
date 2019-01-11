d75a66b98083cefa3b008b8f4bfd066db439b394
Mohammed Fathy <dev.mfathy@gmail.com>
2019-01-11 22:18:41 +0300

    refactor: migrates support library to androidX

--- app/src/main/java/me/mfathy/movies/rater/MoviesActivity.kt
+++ app/src/main/java/me/mfathy/movies/rater/MoviesActivity.kt
@@ -1,6 +1,6 @@
 package me.mfathy.movies.rater
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MoviesActivity : AppCompatActivity() {
